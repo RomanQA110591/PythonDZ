58d8ae565cb8bcdde44be498cc5242bfdca7b898
RomanQA110591 <74190466+RomanQA110591@users.noreply.github.com>
2022-01-03 19:30:46 +0300

    Add files via upload
    DZ_8

---
+++ test/test_add_group.py
@@ -0,0 +1,9 @@
+from model.group import Group
+
+
+def test_add_group(app):
+    app.group.create(Group(name="test", header="test", footer="test"))
+
+
+def test_add_empty_group(app):
+    app.group.create(Group(name="", header="", footer=""))
